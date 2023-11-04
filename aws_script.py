8784d79ae7db777d070eb0b3d1c88f467bef56fe
Br0kenNPC <br0kennpc@Mihail.lan>
2023-11-04 21:34:53 +0100

    add aws script

--- aws_script.py
+++ aws_script.py
@@ -1,6 +1,11 @@
 import pandas as pd
+import time 
 
 lst = pd.read_json('data/list.json', lines=True)
-lst.tags.explode().value_counts().head(5)
 
-print(lst)
+start_time = time.time()
+tags = lst.tags.explode().value_counts().head(5)
+end_time = time.time()
+print("Time: ", str(end_time - start_time))
+
+print(tags)
